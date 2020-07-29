43d5fedc412c7a296d8226f3a4216ec73c505951
Adam C <adam@deniapps.com>
2020-07-29 12:22:10 -0400

    fixed javascript tagging

--- next/components/Common/Meta.js
+++ next/components/Common/Meta.js
@@ -41,18 +41,17 @@ const Meta = props => {
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
-        ></script>
+        />
       )}
       {process.env.NEXT_PUBLIC_ANALYTICS_ID && (
-        <script>
-          dangerouslySetInnerHTML=
-          {{
+        <script
+          dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');`
           }}
-        </script>
+        />
       )}
     </Head>
   );
