902e83b3c9e4497e6d8177e1aacf19ba69f7ce2b
Adam C <adam@deniapps.com>
2021-03-20 13:58:20 -0400

    remove commend

--- next/lib/upload.js
+++ next/lib/upload.js
@@ -12,29 +12,4 @@ const upload = (file) => {
   });
 };
 
-// const getToken = () => {
-//   const deniUser =
-//     typeof localStorage !== "undefined"
-//       ? localStorage.getItem(process.env.NEXT_PUBLIC_USER_LC_KEY)
-//       : "";
-
-//   if (deniUser && deniUser !== "undefined") {
-//     const deniUserObj = JSON.parse(deniUser);
-//     return deniUserObj.accessToken ? deniUserObj.accessToken : "";
-//   }
-//   return "";
-// };
-
-// const upload = (file) => {
-//   return axios
-//     .post(process.env.NEXT_PUBLIC_API_HOST + "/upload", file, {
-//       headers: {
-//         Authorization: getToken(),
-//       },
-//     })
-//     .then((res) => {
-//       return res.data;
-//     });
-// };
-
 export default upload;
