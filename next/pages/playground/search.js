31e173910f08a16ac264f479b6c258ee41657ee6
Adam C <adam@deniapps.com>
2022-01-03 11:56:24 -0500

    added search

---
+++ next/pages/playground/search.js
@@ -0,0 +1,125 @@
+import React from "react";
+import _ from "lodash";
+import faker from "faker";
+import Layout from "components/Layout";
+import { Search, Grid, Header, Segment, Label } from "semantic-ui-react";
+import Link from "next/link";
+
+const title = "NextJS Search Example - Deni Apps";
+const desc = `A simple example to demostrate how to add Search to your website`;
+
+const summary = desc;
+const canonical = "https://deniapps.com/playground/search";
+const image = "https://deniapps.com/images/dna.png";
+
+const seoData = {
+  title,
+  desc,
+  summary,
+  canonical,
+  image,
+};
+
+const source = _.times(5, () => ({
+  title: faker.company.companyName(),
+  description: faker.company.catchPhrase(),
+  image: faker.internet.avatar(),
+  price: faker.finance.amount(0, 100, 2, "$"),
+}));
+
+const initialState = {
+  loading: false,
+  results: [],
+  value: "",
+};
+
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case "CLEAN_QUERY":
+      return initialState;
+    case "START_SEARCH":
+      return { ...state, loading: true, value: action.query };
+    case "FINISH_SEARCH":
+      return { ...state, loading: false, results: action.results };
+    case "UPDATE_SELECTION":
+      return { ...state, value: action.selection };
+
+    default:
+      throw new Error();
+  }
+}
+
+const resultRenderer = ({ title }) => (
+  <Link href="/">
+    <a>{title}</a>
+  </Link>
+);
+
+function SearchExampleStandardCustom() {
+  const [state, dispatch] = React.useReducer(exampleReducer, initialState);
+  const { loading, results, value } = state;
+
+  const timeoutRef = React.useRef();
+  const handleSearchChange = React.useCallback((e, data) => {
+    clearTimeout(timeoutRef.current);
+    dispatch({ type: "START_SEARCH", query: data.value });
+
+    timeoutRef.current = setTimeout(() => {
+      if (data.value.length === 0) {
+        dispatch({ type: "CLEAN_QUERY" });
+        return;
+      }
+
+      const re = new RegExp(_.escapeRegExp(data.value), "i");
+      const isMatch = (result) => re.test(result.title);
+
+      dispatch({
+        type: "FINISH_SEARCH",
+        results: _.filter(source, isMatch),
+      });
+    }, 300);
+  }, []);
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  return (
+    <Layout seoData={seoData}>
+      <h1>Search</h1>
+      <Grid>
+        <Grid.Column width={6}>
+          <Search
+            loading={loading}
+            onResultSelect={(e, data) =>
+              dispatch({
+                type: "UPDATE_SELECTION",
+                selection: data.result.title,
+              })
+            }
+            onSearchChange={handleSearchChange}
+            resultRenderer={resultRenderer}
+            results={results}
+            value={value}
+          />
+        </Grid.Column>
+
+        <Grid.Column width={10}>
+          <Segment>
+            <Header>State</Header>
+            <pre style={{ overflowX: "auto" }}>
+              {JSON.stringify({ loading, results, value }, null, 2)}
+            </pre>
+            <Header>Options</Header>
+            <pre style={{ overflowX: "auto" }}>
+              {JSON.stringify(source, null, 2)}
+            </pre>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </Layout>
+  );
+}
+
+export default SearchExampleStandardCustom;
