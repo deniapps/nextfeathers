4057e6d73bffdd212a212aae9aa8de60fddcc997
Adam C <adam@deniapps.com>
2022-03-06 15:37:45 -0500

    fix scrolling

--- next/next.config.js
+++ next/next.config.js
@@ -10,13 +10,19 @@ module.exports = {
           limit: 8192,
           publicPath: "/_next/static/",
           outputPath: "static/",
-          name: "[name].[ext]"
-        }
-      }
+          name: "[name].[ext]",
+        },
+      },
     });
     // Here is the magic
     // We push our config into the resolve.modules array
     config.resolve.modules.push(path.resolve("./"));
     return config;
-  }
+  },
+  // https://github.com/vercel/next.js/discussions/6009  - we deploy while the production is running 2/14
+  cleanDistDir: false,
+  // https://mmazzarolo.com/blog/2021-04-10-nextjs-scroll-restoration/ - nextjs seems to mess up the scroll to top
+  experimental: {
+    scrollRestoration: true,
+  },
 };
