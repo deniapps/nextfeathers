6aa4b91fd66d34d00194b5a1330781552a89a8cd
Adam C <adam@deniapps.com>
2022-03-06 15:20:48 -0500

    let's see overflow-x filx fix scroll to top issue

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -25,11 +25,11 @@ export default class deniApp extends App {
 
   handleRouteChange = (url) => {
     gtag.pageview(url);
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
+    // window.scroll({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: "smooth",
+    // });
   };
 
   componentDidMount = async () => {
